a37de9bed41b9a7e597be87ba4552feb7c3b9965
Joachim Breitner <mail@joachim-breitner.de>
2012-09-19 07:36:06 +0000

    Make ghc-dup GHCi compatible

--- cbits/dup.c
+++ cbits/dup.c
@@ -1,7 +1,8 @@
 #include "Rts.h"
 
-char  *            info_type       ( StgClosure *closure );
-extern void        printObj        ( StgClosure *obj );
+// Not available in GHCi
+//char  *            info_type       ( StgClosure *closure );
+//extern void        printObj        ( StgClosure *obj );
 
 StgWord dupClosureSize(StgClosure *closure) {
     ASSERT(LOOKS_LIKE_CLOSURE_PTR(closure));
@@ -10,13 +11,14 @@ StgWord dupClosureSize(StgClosure *closure) {
 
 StgWord dupHeapAlloced (StgClosure *closure) {
     ASSERT(LOOKS_LIKE_CLOSURE_PTR(closure));
-    return HEAP_ALLOCED(closure);
+    //return HEAP_ALLOCED(closure); does not work in GHCI
+    return !closure_STATIC(closure);
 }
 
 void dupWarning(StgClosure *closure) {
-    fprintf(stderr,"Static closure passed to dup:\n");
-    fprintf(stderr,"Type: %s\n", info_type(closure));
-    printObj(closure);
+    fprintf(stderr,"Static closure passed to dup!\n");
+    //fprintf(stderr,"Type: %s\n", info_type(closure));
+    // printObj(closure);
 }
 
 void dupDebugPtr(StgWord word) {
